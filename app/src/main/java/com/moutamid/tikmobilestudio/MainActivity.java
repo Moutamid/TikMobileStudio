578bee9e6bd5d3dd57ab74f9f1e0549296cd83d0
suleman <sunnybajwa904@gmail.com>
2022-11-11 20:51:07 +0500

    Initial commit

--- app/src/main/java/com/moutamid/tikmobilestudio/MainActivity.java
+++ app/src/main/java/com/moutamid/tikmobilestudio/MainActivity.java
@@ -3,12 +3,20 @@ package com.moutamid.tikmobilestudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        start = findViewById(R.id.btnStart);
     }
 }
